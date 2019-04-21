e9dea7c0cf9bd1142d5a15153662f1b5c86ef931
Mariya <mariya.djakonova@yandex.ru>
2019-04-21 19:40:59 +0300

    Добавление преподавателя в бд и вывод в таблицы преподавателей и соответствубщих ему предметов

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Applic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApplication1
+{
+    class Applic
+    {
+        public int getScalarCount(string query, MySqlConnection con)
+        {
+            MySqlCommand command = new MySqlCommand(query, con);
+            object count = command.ExecuteScalar();
+            return Convert.ToInt32(count);
+        }
+        public void getSqlWithAliasFor2Object(object titleFirstObj, string FirstObjAlias, object titleSecondObj, string SecondObjAlias, string query, MySqlConnection con)
+        {
+            var command = new MySqlCommand(query, con);
+            command.Parameters.AddWithValue(FirstObjAlias, titleFirstObj);
+            command.Parameters.AddWithValue(SecondObjAlias, titleSecondObj);
+            command.ExecuteNonQuery();
+        }
+
+        public void getSqlWithAliasFor3Object(object titleFirstObj, string FirstObjAlias, object titleSecondObj, string SecondObjAlias, object titleThirdObj, string ThirdObjAlias, string query, MySqlConnection con)
+        {
+            var command = new MySqlCommand(query, con);
+            command.Parameters.AddWithValue(FirstObjAlias, titleFirstObj);
+            command.Parameters.AddWithValue(SecondObjAlias, titleSecondObj);
+            command.Parameters.AddWithValue(ThirdObjAlias, titleThirdObj);
+            command.ExecuteNonQuery();
+        }
+ 
+
+        public DataTable fillDataTable(string query, MySqlConnection con, ref MySqlDataAdapter da, ref DataSet ds)
+        {
+            da = new MySqlDataAdapter(query, con);
+            ds = new DataSet();
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+
+    }
+}
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -8,8 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using Microsoft.Office.Interop.Excel;
-
+using WindowsFormsApplication1;
 
 
 namespace TimeTableProject
@@ -17,25 +16,20 @@ namespace TimeTableProject
     public partial class Form1 : Form
     {
         private MySqlConnection connect;
-        private MySqlDataAdapter commonDataAdapter;
-        static int changedRow, changedColumn, changedDiscipline;
-        private int myIndex, ringIndex, discIndex;
-        string[] discArray, roomArray, lecturerArray;
+        private MySqlDataAdapter commonDataAdapter; //заполняет DataSet данными из БД
+        static int changedRow;
+        private int myIndex;
 
+        Applic app = new Applic();
+  
+        private DataSet commonDataSet, disciplineDataSet;  //хранилище данных
 
-       
-        private DataSet commonDataSet, disciplineDataSet;
-     
-
-        DataGridView[] dgvWeekArray;
-        List<int> ringIDList, groupIDList, dayIDList;
+        //DataGridView[] dgvWeekArray;
         public Form1()
         {
             InitializeComponent();
             changedRow = -1;
             myIndex = -1;
-            changedColumn = -1;
-            ringIndex = -1;
 
             try
             {
@@ -52,25 +46,94 @@ namespace TimeTableProject
          
         }
 
-            private void button_addLect_Click(object sender, EventArgs e)
+        #region Преподаватель (Lecturer) 
+
+        private DataSet searchSomeDataForOneLecturer(string query, MySqlConnection con, int lectID, string alias) 
         {
+            MySqlDataReader reader;
+            DataSet ds = new DataSet();
+            DataTable table = new DataTable();
+            MySqlCommand command = new MySqlCommand(query, con);  // экземпляр класса MySqlCommand с текстом запроса и MySqlConnection
+            command.Parameters.AddWithValue(alias, lectID);       //преобразует string в int lectID
+            reader = command.ExecuteReader();     //Отправляет CommandText в соединение и создает MySqlDataReader
+            table.Load(reader);
+            ds.Merge(table);
+            return ds;
+        }
+
+
+
+        private void dGV_lecturer_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                changedRow = dGV_Lecturer.SelectedCells[0].RowIndex;
+                myIndex = Convert.ToInt32(dGV_Lecturer.Rows[changedRow].Cells[0].Value);
+                label_helpLectID.Text = myIndex.ToString();      //индекс препода выбранной ячейки
+                tB_lectSurname.Text = dGV_Lecturer.Rows[changedRow].Cells[1].Value.ToString();
+                tb_lectName.Text = dGV_Lecturer.Rows[changedRow].Cells[2].Value.ToString();
+                tB_lectPatro.Text = dGV_Lecturer.Rows[changedRow].Cells[3].Value.ToString();
+                disciplineDataSet = searchSomeDataForOneLecturer(Queries.selectDisciplineForOneLecturer, connect, myIndex, "@discLectID");
+                dGV_DiscForLecturer.DataSource = disciplineDataSet.Tables[0];
+                dGV_DiscForLecturer.Columns[0].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
 
-        private void button_addLesson_Click(object sender, EventArgs e)
+        private void button_addDiscForLect_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void button_addLect_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (tb_lectName.Text != "" && tB_lectSurname.Text != "" && tB_lectPatro.Text != "")
+                {
+                    try
+                    { 
+                    app.getSqlWithAliasFor3Object(tb_lectName.Text, "@lectName", tB_lectSurname.Text, "@lectSurname", tB_lectPatro.Text, "@lectPatro", Queries.insertLecturer, connect);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    dGV_Lecturer.DataSource = app.fillDataTable(Queries.selectLecturer, connect, ref commonDataAdapter, ref commonDataSet);
+                    dGV_Lecturer.Columns[0].Width = 30;
 
+                    tb_lectName.Clear();
+                    tB_lectSurname.Clear();
+                    tB_lectPatro.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Заполните поля!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
-        private void button_excel_Click(object sender, EventArgs e)
+        private void dGV_Lecturer_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+           
         }
-
     }
+
+    #endregion
+
+    #region Общие методы (common methods)
+   
+        
+
+
+    #endregion
+
 }
+
